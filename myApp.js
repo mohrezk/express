b40d1b44f4d615b2027c5903162bdd2d12b01762
mohrezk <moh.rezkk0@gmail.com>
2022-09-25 16:23:49 +0200

    finished

--- myApp.js
+++ myApp.js
@@ -1,6 +1,10 @@
 let express = require('express');
 let app = express();
-// require('dotenv').config();
+require('dotenv').config();
+
+// challenge 11
+let bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(function(req, res, next){
         console.log(req.method + " " + req.path + " - " + req.ip);
@@ -31,6 +35,7 @@ app.get('/json',
         res.json(o)
     })
 
+// challenge 8
 app.get('/now',
     function(req, res, next)
     {
@@ -43,6 +48,25 @@ app.get('/now',
     }
     )
 
+// challenge 9
+app.get('/:word/echo',
+    function(req, res)
+    {
+        res.json({"echo": req.params.word})
+    })
+// challend 10
+app.get('/name',
+    function(req, res)
+    {
+        res.json({"name": req.query.first + " " +  req.query.last})
+    }
+)
+app.post('/name',
+    function(req, res)
+    {
+        res.json({"name": req.body.first + " " + req.body.last})
+    }
+)
 // console.log("Hello World")
 
 
